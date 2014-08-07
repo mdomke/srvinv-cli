f1f36dce462a8778e19537bf5e014219621d130e
perfide <github.perfide@safersignup.com>
2014-08-07 13:52:34 +0200

    [CHANGE] do not use json internally [FIX] do not fail on empty srvinv replies

--- libsrvinv/__init__.py
+++ libsrvinv/__init__.py
@@ -39,25 +39,22 @@ class SrvinvCache:
     else:
       self.d_times[s_resource] = time.time() + self.i_cache_duration_in_s
       if self.b_cache_use_file:
-        s_json_reply = self.fetch(s_resource, b_as_json=True)
+        s_json_reply = self.fetch(s_resource)
         with open(s_cache_file_path, 'w') as fp:
           fp.write(s_json_reply)
           os.chmod(s_cache_file_path, 0o766)
           return json.loads(s_json_reply)
       else:
-        x_reply = self.fetch(s_resource, b_as_json=False)
+        x_reply = self.fetch(s_resource)
         self.d_cache[s_resource] = x_reply
         return x_reply
     return None
   # end def get
 
-  def fetch(self, s_resource, b_as_json):
-    (i_status_code, s_reply) = _request_srvinv('get', s_resource)
+  def fetch(self, s_resource):
+    (i_status_code, x_reply) = _request_srvinv('get', s_resource)
     if i_status_code == 200:
-      if b_as_json:
-        return s_reply
-      else:
-        return json.loads(s_reply)
+      return x_reply
     else:
       return None
   # end def fetch
@@ -67,7 +64,7 @@ go_srvinv_cache = SrvinvCache()
 
 def _request_srvinv(rtype, resource, resourceid=None, attribute=None, data=None):
   """builds a url and sends a requests to srvinv
-  returns a tuple of http-status-code and text-reply
+  returns a tuple of http-status-code and json-parsed
   on connection error the code is set to -1"""
   if not resource.endswith('s'):
     resource += 's'
@@ -79,11 +76,17 @@ def _request_srvinv(rtype, resource, resourceid=None, attribute=None, data=None)
   try:
     apirequest = session.request(rtype, s_url, data=data)
     i_status_code = apirequest.status_code
-    s_reply = apirequest.text
+    if apirequest.text:
+      x_reply = json.loads(apirequest.text)
+    else:
+      x_reply = None
   except requests.exceptions.ConnectionError:
     i_status_code = -1
-    s_reply = ''
-  return (i_status_code, s_reply)
+    x_reply = None
+  except ValueError: # from json.loads
+    i_status_code = -2
+    x_reply = None
+  return (i_status_code, x_reply)
 # end def _request_srvinv
 
 def get(resource, resourceid, attribute):
@@ -92,26 +95,25 @@ def get(resource, resourceid, attribute):
   1 if the resource does not exist
   2 if the attribute does not exist
   3 on error"""
-  (i_status_code, s_reply) = _request_srvinv('get', resource, resourceid)
+  (i_status_code, x_reply) = _request_srvinv('get', resource, resourceid)
   if i_status_code == 200:
     if not attribute:
-      return (0, s_reply)
+      return (0, x_reply)
     else:
-      resource_as_obj = json.loads(s_reply)
-      if not attribute in resource_as_obj:
-        return (2, '')
-      return (0, json.dumps(resource_as_obj[attribute]))
+      if not attribute in x_reply:
+        return (2, None)
+      return (0, x_reply[attribute])
   elif i_status_code == 404:
-    return (1, '')
+    return (1, None)
   #elif i_status_code == 500:
-  return (3, '')
+  return (3, None)
 # end def get
 
-def set(resource, resourceid, attribute, value, use_json=True):
+def set(resource, resourceid, attribute, value):
   """sets a attribut of a existing object
   return 0 on succes, 1/2 if first/second connections fails
   and 3 is the object does not exist"""
-  (i_status_code, s_reply) = _request_srvinv('get', resource, resourceid)
+  i_status_code = _request_srvinv('get', resource, resourceid)[0]
   if i_status_code == 200:
       # validate if value is json so we dont put it in there as string
       try:
@@ -123,7 +125,7 @@ def set(resource, resourceid, attribute, value, use_json=True):
         # is unparseable string
         pass
       to_set_value = json.dumps({"value": value})
-      (i_status_code, s_reply) = _request_srvinv('patch', resource, resourceid, attribute, data=to_set_value)
+      i_status_code = _request_srvinv('patch', resource, resourceid, attribute, data=to_set_value)[0]
       if i_status_code == 202:
         return 0
       else:
@@ -138,7 +140,7 @@ def register(resource, resourceid):
   retuns 0 on success, 1 if the name exists and 2 on error"""
   to_register_resource = {"name": resourceid, "created_at": datetime.utcnow().isoformat(), "updated_at": datetime.utcnow().isoformat()}
   to_register_resource = json.dumps(to_register_resource)
-  (i_status_code, s_reply) = _request_srvinv('post', resource, data=to_register_resource)
+  i_status_code = _request_srvinv('post', resource, data=to_register_resource)[0]
   if i_status_code == 201:
     return 0
   elif i_status_code == 409:
@@ -150,14 +152,14 @@ def register(resource, resourceid):
 def delete(resource, resourceid):
   """deletes the given resource-name in the given resource
   returns 0 on success and 1 on errror"""
-  (i_status_code, s_reply) = _request_srvinv('delete', resource, resourceid)
+  i_status_code = _request_srvinv('delete', resource, resourceid)[0]
   if i_status_code == 202:
     return 0
   else:
     return 1
 # end def delete
 
-def search(resource, attribute, value, use_json=True):
+def search(resource, attribute, value):
   """searches for objects with the given attribute/value combination
   value supports wildcards
   use_json: return a py-list if set to False, else return a json-string
@@ -175,8 +177,6 @@ def search(resource, attribute, value, use_json=True):
          attribute_to_search = resource_to_search[attribute]
       if fnmatch.fnmatch(str(attribute_to_search), value):
         found_resources.append(resource_to_search)
-  if use_json:
-    return json.dumps(found_resources)
   return found_resources
 # end def search
 
