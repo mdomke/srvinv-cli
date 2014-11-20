b93da31fa04666e0ffde2f2e6312bf86d1d2bd39
Martin Domke <martin.domke@project-collins.com>
2014-11-20 16:07:44 +0100

    Also read config from /opt/collins

--- libsrvinv/config.py
+++ libsrvinv/config.py
@@ -7,5 +7,6 @@ config = ConfigParser()
 config.read([
     os.path.join(sys.prefix, "etc/collins/srvinv.cfg"),
     "/etc/collins/srvinv.cfg",
+    "/opt/collins/srvinv.cfg",
     os.path.expanduser("~/.config/srvinv.cfg"),
     "srvinv.cfg"])
